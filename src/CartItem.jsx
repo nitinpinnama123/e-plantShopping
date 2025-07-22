afa492cd9f103a858d2ebadabdbe9b920c854e94
Nitin Pinnamaneni <nitinpinnamaneni@yahoo.com>
2025-07-22 15:21:40 -0400

    Product List component layout, state management using redux, and cart items component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import CartSlice, { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -9,20 +9,33 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach((item) => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        total += quantity * cost;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1)
+   {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}));
+   }
+   else {
+    dispatch(removeItem({name: item.name}))
   };
 
   const handleRemove = (item) => {
@@ -30,6 +43,12 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    const quantity = item.quantity;
+    const cost = parseFloat(item.cost.substring(1));
+    totalCost = quantity * cost;
+    return totalCost;
+
   };
 
   return (
@@ -64,5 +83,4 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
-
 
